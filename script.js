7dc21bd9929b7770ab317467fd4a7e35ed7c6f38
Vaishnavi11656 <166276503+Vaishnavi11656@users.noreply.github.com>
2026-01-05 00:25:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const color = document.getElementById("colorSelect");
+const button = document.querySelector("input[type='button']");
+
+button.addEventListener("click", function(){
+	const selectedIndex = color.selectedIndex;
+	if(selectedIndex !== -1){
+		color.remove(selectedIndex);
+	}
+});
